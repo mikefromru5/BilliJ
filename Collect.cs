fef7d7a43ccd01c08f288ce10ff41e58b107db64
Mikhail <80206224+mikefromru5@users.noreply.github.com>
2023-01-23 12:32:04 +0300

    Add files via upload

---
+++ Collect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collect : MonoBehaviour
+{
+    public static int TheCherry;
+    public Text TextCherry;
+
+    void Start()
+    {
+        TextCherry = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextCherry.text = "x" + TheCherry;
+    }
+}
---
+++ Hero.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] float speed = 3f;
+    [SerializeField] float jumpForce = 1f;
+    bool Ground = false;
+
+    Rigidbody2D rb;
+    SpriteRenderer sprite;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+            Run();
+        if (Ground && Input.GetButton("Jump"))
+            Jump();
+    }
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Run()
+    {
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+
+        sprite.flipX = dir.x < 0.0f;
+    }
+
+    void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    void Grounded()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 1f);
+        Ground = collider.Length > 1;
+    }
+
+    void FixedUpdate()
+    {
+        Grounded();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+            if (collision.tag == "Cherry")
+        {  Collect.TheCherry += 1;
+            Destroy(collision.gameObject);
+        }
+        
+          else  if (collision.tag == "qwe")
+            {
+                SceneManager.LoadScene(0);
+            }
+        
+
+    }
+}
